312e3c2064a4d8556260548a3918786d835074eb
Jason Clancey <jason.clancey@dal.ca>
2022-01-22 17:34:21 -0700

    added functionality for the reset button, choosing size of etch-a-sketch, reorganized some other code into functions

--- script.js
+++ script.js
@@ -1,15 +1,53 @@
-// Create 16x16 grid of square divs
+// Create 16x16 grid of square divs as the default size of the etch-a-sketch pad
+
+let sideOfSquare = 16;
 
 const container = document.querySelector('.container');
 const gridSquare = document.createElement('div');
+
 gridSquare.setAttribute('id', 'theGrid')
 
-for (let i = 0; i < 256; i++) {
-    container.appendChild(gridSquare.cloneNode(true))
-};
+// Function Let user input a new value for each side of the etch-a-sketch pad
+
+function makeGrid(sideOfSquare) {
+    for (let i = 0; i < (sideOfSquare ** 2); i++) {
+        container.appendChild(gridSquare.cloneNode(true))
+}};
+
+
+function changeTheGrid() {
+    const changeColor = document.querySelectorAll('#theGrid');
+    
+    changeColor.forEach(square => {
+        square.addEventListener('mouseover', () => 
+            square.style.backgroundColor = 'purple')
+    })
+
+    changeColor.forEach(square => 
+        square.style.width = `${(1 / sideOfSquare * 100)}%`)
+    
+    changeColor.forEach(square => 
+        square.style.height = `${(1 / sideOfSquare * 100)}%`)    
+}
+
+makeGrid(sideOfSquare)
+changeTheGrid()
+
 
 const grid = document.querySelectorAll('#theGrid');
+const resetButton = document.querySelector('.userInput');
 
-grid.forEach(square => {
-    square.addEventListener('mouseover', () => square.style.backgroundColor = 'purple')
+resetButton.addEventListener('click', () => {
+    sideOfSquare = prompt("Please enter the number of squares per side (max 100)", '');
+    if (sideOfSquare > 100) {
+            sideOfSquare = 100;
+}
+    while (container.firstChild) {
+        container.removeChild(container.firstChild)
+    }
+    makeGrid(sideOfSquare);
+    changeTheGrid()
 })
+
+
+
